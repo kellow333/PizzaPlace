6001e5f9974e0600a76ba714f0509aac748324ed
kellow <robertkellow@hotmail.co.uk>
2017-02-08 14:41:10 +0000

    PizzaPlace
    I love pizza, who doesn't? .NET web app

---
+++ PizzaPlace/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PizzaPlace.Models
+{
+    public class Category
+    {
+        [ScaffoldColumn(false)]
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Category Description")]
+        public string CategoryDescription { get; set; }
+
+        public virtual ICollection<Pizza> Pizza { get; set; }
+    }
+}
---
+++ PizzaPlace/Models/Pizza.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Models
+{
+    public class Pizza
+    {
+        [ScaffoldColumn(false)]
+        public int PizzaID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string PizzaName { get; set; }
+
+        [Required, StringLength(1000), Display(Name = "Pizza Description")]
+        public string PizzaDescription { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
